eb3f8fc75ec6549d4e96d8e7fd22d087892f74f4
Timhung803 <timhkt@gmail.com>
2022-05-02 02:06:20 +1200

    Navigatting Programmatically

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
   function addMeetupHandler(meetupData) {
     fetch(
-      
+      '',
       {
         method: "POST",
         body: JSON.stringify(meetupData),
@@ -11,7 +13,9 @@ function NewMeetupPage() {
           "Content-Type": "application/json",
         },
       }
-    );
+    ).then (() => {
+      navigate('/',{replace:true});
+    });
   }
   return (
     <section>
